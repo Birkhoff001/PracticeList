da902d959f148ada48d2966d51a950b82e8df576
Birkhoff001 <1276267330@qq.com>
2017-04-25 18:03:31 +0800

    changed employee.java

--- Employee.java
+++ Employee.java
@@ -1,6 +1,10 @@
 package PracticeJava;
-/*定义并测试一个employee类，员工属性包括编号，姓名，基本薪水，薪水增长额，
- * 包括计算薪水增长额及计算增长后的工资总额的操作方法
+
+import java.util.Scanner;
+
+/*1.定义并测试一个employee类，员工属性包括编号，姓名，基本薪水，薪水增长额，
+ * 包括计算薪水增长额及计算增长后的工资总额的操作方法。
+ * 2.统计出一串字符串中某个字母出现的次数。
  */
 class EmployeeInfo {
 	private String id;
@@ -47,10 +51,39 @@ class EmployeeInfo {
 		System.out.println("the " + this.id + " " + this.name + "'s addAfterSalary: " + (this.basesalary + this.addsalary));
 	}
 }
+class Str_Count{
+	private int count;
+	private char input[]; 
+	private char charac;
+	public Str_Count(String input, char charac){
+		this.input = input.toCharArray();
+		this.charac = charac;
+		input();
+	}
+	@SuppressWarnings("resource")
+	public void input(){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Input string array: ");
+		String string = scanner.nextLine();
+		this.input = string.toCharArray();
+	}
+	public void print(){
+		for(int i = 0; i<this.input.length; i++){
+			if(input[i] == this.charac){
+				this.count++;
+			}
+		}
+		System.out.println("the number " + this.charac + " appear " 
+				+ this.count);
+		this.count = 0;
+	}
+}
 public class Employee{
 	public static void main(String[] args){
-		EmployeeInfo e = new EmployeeInfo("1001", "Bob", 10000, 1000);
+		EmployeeInfo e = new EmployeeInfo("1001", "Bob", 20000, 1000);
 		e.print_baseemployee();
 		e.print_addafteremployee();
+		Str_Count c = new Str_Count("", 'h');
+		c.print();
 	}
 }
